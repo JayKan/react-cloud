a08c738b6909d47a845daff460fd5a06d955d60c
Jay Kan <leicasper@gmail.com>
2017-06-29 15:34:36 -0700

    chore: add express static server

---
+++ server/config/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const compression = require('compression');
+const express = require('express');
+const favicon = require('serve-favicon');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const paths = require('./paths');
+
+module.exports = app => {
+  // server address
+  app.set('host', process.env.HOST || 'localhost');
+  app.set('port', process.env.PORT || 4000);
+
+  // configure express to use bodyParser as middleware
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+
+  // HTTP headers
+  app.disable('x-powered-by');
+  app.use(helmet.frameguard({ action: 'deny' }));
+  app.use(helmet.hsts({ force: true, maxAge: 7776000000 })); // 90 days
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+  app.use(helmet.ieNoOpen());
+
+  // gzip compression
+  app.use(compression());
+
+  // development mode only
+  if (process.env.NODE_ENV === 'development') {
+    app.use(require('morgan')('dev'));
+  }
+
+  // static files
+  app.use(express.static(paths.static, { index: false }));
+  app.use(favicon(paths.favicon));
+};
