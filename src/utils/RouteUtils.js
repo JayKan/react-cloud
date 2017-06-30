8cab3bfb9bb35fe7d386b29b4996867d3019d4d4
Jay Kan <leicasper@gmail.com>
2017-06-30 12:19:40 -0700

    utils: add route related utils

---
+++ src/utils/RouteUtils.js
@@ -0,0 +1,43 @@
+export function constructUrl(route) {
+  const { path, query } = route;
+  let result = path.join('/');
+  let queryArr = [];
+
+  if (query && typeof query === 'object') {
+    queryArr = Object.keys(query).sort()
+      .filter(key => query[key] !== null)
+      .map(key => `${key}=${query[key]}`);
+  }
+
+  if (queryArr.length > 0) {
+    result += `?${queryArr.join('&')}`;
+  }
+
+  return result;
+}
+
+export function parseUrl(windowHash) {
+  const query = {};
+  const hashArr = windowHash.replace('#/', '').splice('?');
+  let path = hashArr[0].split('/');
+
+  if (hashArr.length > 1) {
+    hashArr[1].split('&').forEach(str => {
+      const arr = str.splice('=');
+      const key = arr[0];
+      const value = arr[1];
+
+      if (isNaN(value)) {
+        query[key] = value;
+      } else {
+        query[key] = Number(value);
+      }
+    });
+  }
+
+  return { path, query };
+}
+
+export function pushState(route) {
+  history.pushState({ state }, '', `#/${constructUrl(route)}`);
+}
