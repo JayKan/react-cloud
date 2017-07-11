9b15a545ba8e4712bc794774b4495758f914531f
Jay Kan <leicasper@gmail.com>
2017-07-10 17:43:46 -0700

    feat(components): add Me component

---
+++ src/components/Me.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { fetchSongsIfNeeded } from '../actions/PlaylistsActions';
+import MePromptStream from './MePromptStream';
+import MePromptLikes from './MePromptLikes';
+import SongCards from './SongCards';
+import { AUTHED_PLAYLIST_SUFFIX } from '../constants/PlaylistConstants';
+
+const propTypes = {
+  authed: PropTypes.object.isRequired,
+  authedPlaylists: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  height: PropTypes.number.isRequired,
+  playingSongId: PropTypes.number,
+  playlists: PropTypes.object.isRequired,
+  route: PropTypes.object.isRequired,
+  songs: PropTypes.object.isRequired,
+  users: PropTypes.object.isRequired,
+};
+
+class Me extends React.Component {
+  getPlaylist() {
+    const { authedPlaylists, route } = this.props;
+    const { path } = route;
+
+    switch (path[1]) {
+      case 'stream':
+        return 'stream';
+      case 'likes':
+        return 'likes';
+      case 'playlists': {
+        if (path.length < 3 || !(path[2] in authedPlaylists)) {
+          return 'playlists';
+        }
+        const playlist = authedPlaylists[path[2]];
+        return playlist.title;
+      }
+      default:
+        return 'stream';
+    }
+  }
+
+  renderPrompt() {
+    const { authed, dispatch, playlists } = this.props;
+
+    switch (this.getPlaylist()) {
+      case 'stream':
+        return (
+          <MePromptStream
+            authed={ authed }
+            dispatch={ dispatch }
+          />
+        );
+      case 'likes':
+        return (
+          <MePromptLikes
+            authed={ authed }
+            dispatch={ dispatch }
+            playlists={ playlists }
+          />
+        );
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    const { authed, dispatch, height, playingSongId, playlists, songs, users } = this.props;
+    const playlist = this.getPlaylist() + AUTHED_PLAYLIST_SUFFIX;
+
+    return (
+      <div className="me">
+        { this.renderPrompt() }
+        <div className="container">
+          <SongCards
+            authed={ authed }
+            dispatch={ dispatch }
+            height={ height }
+            playingSongId={ playingSongId }
+            playlist={ playlist }
+            playlists={ playlists }
+            scrollFunc={ fetchSongsIfNeeded.bind(null, playlist) }
+            songs={ songs }
+            users={ users }
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+Me.propTypes = propTypes;
+
+export default Me;
