ff9d29b721c071cdd584784d21e972c4ed2bdca7
Jay Kan <leicasper@gmail.com>
2017-07-10 17:44:47 -0700

    feat(containers): add Song container

---
+++ src/containers/SongContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const propTypes = {
+
+};
+
+class SongContainer extends React.Component {
+  render() {
+
+  }
+}
+
+SongContainer.propTypes = propTypes;
+
+export default SongContainer;
