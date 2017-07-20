f2e2810224e273c7500bc57c883ab929224982c4
Jay Kan <leicasper@gmail.com>
2017-07-20 00:40:24 -0700

    feat(components): add MobilePlayer component

---
+++ src/components/MobilePlayer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+
+};
+
+class MobilePlayer extends React.Component {
+  render() {
+
+  }
+}
+
+MobilePlayer.propTypes = propTypes;
+
+export default MobilePlayer;
