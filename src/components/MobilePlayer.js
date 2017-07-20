c4b23523e4b1de2df003cf597880404433201854
Jay Kan <leicasper@gmail.com>
2017-07-20 00:45:00 -0700

    refactor(components): update MobilePlayer component

--- src/components/MobilePlayer.js
+++ src/components/MobilePlayer.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Motion, spring } from 'react-motion';
+import MobilePlayerContent from './MobilePlayerContent';
 
 const propTypes = {
-
+  playingSongId: PropTypes.number,
 };
 
 class MobilePlayer extends React.Component {
-  render() {
+  renderPlayerContent() {
+    const { playingSongId } = this.props;
+    if (playingSongId === null) {
+      return <div />;
+    }
 
+    return <MobilePlayerContent { ...this.props } />
+  }
+
+  render() {
+    const { playingSongId } = this.props;
+    const isSongPlaying = playingSongId !== null;
+    return (
+      <Motion style={{ height: spring(isSongPlaying ? 100 : 0) }}>
+        {({ height }) =>
+          <div className="mobile-player-container" style={{ height }}>
+            { this.renderPlayerContent() }
+          </div>
+        }
+      </Motion>
+    );
   }
 }
 
