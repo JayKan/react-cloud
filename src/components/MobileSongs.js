6d58e90bd53b661f2ccb7d05dcf8dcd011d12e33
Jay Kan <leicasper@gmail.com>
2017-08-15 19:00:32 -0700

    feat(compnoents): add MobileSong component

---
+++ src/components/MobileSongs.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MobileSongList from './MobileSongList';
+import { fetchSongsIfNeeded } from '../actions/PlaylistsActions';
+
+const propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  playingSongId: PropTypes.number,
+  playlist: PropTypes.string.isRequired,
+  playlists: PropTypes.object.isRequired,
+  songs: PropTypes.object.isRequired,
+  users: PropTypes.object.isRequired,
+};
+
+class MobileSongs extends React.Component {
+  componentWillMount() {
+    const { dispatch, playlist, playlists } = this.props;
+    if (!(playlist in playlists) || playlists[playlist].items.length === 0) {
+      dispatch(fetchSongsIfNeeded(playlist));
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { dispatch, playlist, playlists } = this.props;
+    if (playlist !== nextProps.playlist) {
+      if (!(nextProps.playlist in playlists) || playlists[nextProps.playlist].items.length === 0) {
+        dispatch(fetchSongsIfNeeded(nextProps.playlist));
+      }
+    }
+  }
+
+  render() {
+    const { dispatch, playingSongId, playlist, playlists, songs, users } = this.props;
+
+    return (
+      <MobileSongList
+        playlist={ playlist }
+        playlists={ playlists }
+        songs={ songs }
+        users={ users }
+        playingSongId={ playingSongId }
+        dispatch={ dispatch }
+      />
+    );
+  }
+}
+
+MobileSongs.propTypes = propTypes;
+
+export default MobileSongs;
